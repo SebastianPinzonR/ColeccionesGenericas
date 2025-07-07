847577646293af0694f19b91dc20a26ab8646f66
Sebastian Pinzon <spinzonrojas@gmail.com>
2025-07-07 12:29:10 -0500

    Primera Coleccion la lista

--- ColeccionesGenericas/Program.cs
+++ ColeccionesGenericas/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> Valores = new List<int>();
+
+
+        Valores.Add(5);
+        Valores.Add(10);
+        Valores.Add(15);
+        Valores.Add(20);
+        Valores.Add(25);
+        Valores.Add(30);
+
+        //Recorremos la lista
+        foreach (int valor in Valores)
+        {
+            //NO se utiliza el Type cast
+            //Es decir nos es necesario convertir
+            //un objet a int o el tipo de dato que se vaya a utilizar
+            Console.WriteLine(valor);
+        }
+        Console.WriteLine("--------------------------------");
+
+        //Validamos si tiene los siguientes este va a arrojar falso o verdadero
+        Console.WriteLine(Valores.Contains(5));
+        Console.WriteLine(Valores.Contains(17));
+
+        Console.WriteLine("----------------------------------");
+
+        //Obtenemos el indice de un elemento
+        Console.WriteLine(Valores.IndexOf(12));
+        //SI este no se encuentra arrojara -1
+        Console.WriteLine(Valores.IndexOf(10));
+        //Si si se encuentra, dara la posicion recordar que esta desde 0 hasta un numero determinado
+
+        Console.WriteLine("-----------------------------------------");
+
+        //Insertamos un elemento en un indice
+        Valores.Insert(6, 35);
+        foreach (int valor in Valores)
+        {
+            Console.WriteLine(valor);
+        }
+        Console.WriteLine("-----------------------------------------------");
+
+        //Eliminamos un elemento dando un indice
+        Valores.RemoveAt(5);
+        foreach (int valor in Valores)
+        {
+            Console.WriteLine(valor);
+        }
+        Console.WriteLine("------------------------------------------------");
+
+        //Eliminamos la primera ocurrencia de un valor
+        Valores.Remove(5);
+    }
+}
--- ColeccionesGenericas/obj/Debug/net9.0/ColeccionesGenericas.AssemblyInfo.cs
+++ ColeccionesGenericas/obj/Debug/net9.0/ColeccionesGenericas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ColeccionesGenericas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b89d4889240de1b80aa9a8965a9f07ad1697844")]
 [assembly: System.Reflection.AssemblyProductAttribute("ColeccionesGenericas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ColeccionesGenericas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
