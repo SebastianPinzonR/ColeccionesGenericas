8b4d203f12f3e68310df68e170a62546e7500d2d
Sebastian <spinzonrojas@gmail.com>
2025-07-08 22:53:21 -0500

    Se creo un nuevo proyecto el cual esta mas enfocado a hacer listas con una clase

---
+++ ColeccionesGenericas1/Program.cs
@@ -0,0 +1,49 @@
+﻿using ColeccionesGenericas1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Lista de objetos de nuestra clase
+        //Adicion explicita, no con add
+        List<Punto> puntos = new List<Punto>
+        {
+            new Punto(2,3),
+            new Punto(5,23),
+            new Punto(7,9)
+        };
+
+        foreach (Punto p in puntos)
+        {
+            Console.WriteLine(p);
+        }
+        Console.WriteLine("--------------------------------------------------");
+
+        //adicionamos un punto
+        puntos.Add(new Punto(2,3));
+
+        foreach (Punto p in puntos)
+        {
+            Console.WriteLine(p);
+        }
+        Console.WriteLine("--------------------------------------------------");
+
+        //Insertamos un punto
+        puntos.Insert(2, new Punto(100,51));
+
+        foreach (Punto p in puntos)
+        {
+            Console.WriteLine(p);
+        }
+        Console.WriteLine("--------------------------------------------------");
+
+        //Copiamos de lista a arreglo
+
+        Punto[] arregloPuntos = puntos.ToArray();
+        for (int n = 0; n < arregloPuntos.Length; n++)
+        {
+            Console.WriteLine(arregloPuntos[n]);
+        }
+        Console.WriteLine("---------------------------------------------------");
+    }
+}
