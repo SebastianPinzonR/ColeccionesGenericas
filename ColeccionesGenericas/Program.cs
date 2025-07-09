066ec7f28a8f34d3bcadcaa8a2b8c81d691abbb3
Sebastian <spinzonrojas@gmail.com>
2025-07-08 22:37:39 -0500

    Se crep una clase para que esta clase se pueda poner en una lista generica

--- ColeccionesGenericas/Program.cs
+++ ColeccionesGenericas/Program.cs
@@ -35,7 +35,7 @@
         //Si si se encuentra, dara la posicion recordar que esta desde 0 hasta un numero determinado
 
         Console.WriteLine("-----------------------------------------");
-
+        Console.WriteLine("Insertamos un elemento en un indice");
         //Insertamos un elemento en un indice
         Valores.Insert(6, 35);
         foreach (int valor in Valores)
@@ -45,6 +45,7 @@
         Console.WriteLine("-----------------------------------------------");
 
         //Eliminamos un elemento dando un indice
+        Console.WriteLine("Eliminamos un elemento dando un indice");
         Valores.RemoveAt(5);
         foreach (int valor in Valores)
         {
@@ -53,6 +54,41 @@
         Console.WriteLine("------------------------------------------------");
 
         //Eliminamos la primera ocurrencia de un valor
+        Console.WriteLine("Eliminamos la primera ocurrencia de un valor");
         Valores.Remove(5);
+        foreach(int valor in Valores)
+            { Console.WriteLine(valor); }
+        Console.WriteLine("--------------------------------------------------");
+
+        //La lista se coloca en sentido contrario
+        Console.WriteLine("Lista desordenada");
+        Valores.Reverse();
+        foreach (int valor in Valores)
+            { Console.WriteLine(valor); }
+        Console.WriteLine("------------------------------------------------------");
+
+        //Mandamos a ordenar la lista
+        //Si invocamos el metodo sort para utilizarlo con una clase, la clase tiene que tener IComparable
+        Console.WriteLine("Lista ordenada");
+        Valores.Sort();
+        foreach (int valor in Valores) {
+            Console.WriteLine(valor); }
+        Console.WriteLine("---------------------------------------------------------");
+
+        //Creamos una lista para nuestra clase
+        List<Punto> listaPuntos = new List<Punto>();
+
+        listaPuntos.Add(new Punto(5, 3));
+        listaPuntos.Add(new Punto(7, 8));
+        listaPuntos.Add(new Punto(12, 5));
+        listaPuntos.Add(new Punto(6, 2));
+
+        foreach (Punto punto in listaPuntos)
+        {
+            Console.WriteLine(punto);
+        }
+
+        //No funciona el siguiente metodo Sort por que no hemos implementado IComparable
+        //listaPuntos.Sort();
     }
 }
--- ColeccionesGenericas/Punto.cs
+++ ColeccionesGenericas/Punto.cs
@@ -1,4 +1,15 @@
 class Punto
 {
-    
+    private int x;
+    private int y;
+    public Punto(int parametroX, int parametroY)
+    {
+        x = parametroX;
+        y = parametroY;
+    }
+
+    public override string ToString()
+    {
+        return string.Format($"X = {x} Y = {y}");
+    }
 }
